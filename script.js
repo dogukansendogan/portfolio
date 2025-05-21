3f52bf6673ec6a56437fe0235c8c280559095779
dogukansendogan <dogukansendogan@posta.mu.edu.tr>
2025-05-21 20:09:41 +0300

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggleBtn = document.getElementById("themeToggle");
-  
-    // Tema durumunu localStorage'dan al
     const savedTheme = localStorage.getItem("theme");
     const isDark = savedTheme === "dark";
   
-    // İlk yüklemede temayı uygula
     if (isDark) {
       document.body.classList.add("dark-mode");
       toggleBtn.innerHTML = '<i class="fas fa-sun"></i> Light Mode';
@@ -13,19 +10,28 @@ document.addEventListener("DOMContentLoaded", () => {
       toggleBtn.innerHTML = '<i class="fas fa-moon"></i> Dark Mode';
     }
   
-    // Tıklama ile tema değiştir
     toggleBtn.addEventListener("click", () => {
       document.body.classList.toggle("dark-mode");
   
       const currentlyDark = document.body.classList.contains("dark-mode");
   
-      // localStorage güncelle
       localStorage.setItem("theme", currentlyDark ? "dark" : "light");
   
-      // Buton metni değiştir
       toggleBtn.innerHTML = currentlyDark
         ? '<i class="fas fa-sun"></i> Light Mode'
         : '<i class="fas fa-moon"></i> Dark Mode';
     });
   });
-  
+  document.addEventListener("DOMContentLoaded", function () {
+  const backBtn = document.querySelector('.back-button');
+
+  if (
+    window.location.pathname.endsWith("index.html") ||
+    window.location.pathname === "/" ||
+    window.location.pathname === "/portfolio/"
+  ) {
+    if (backBtn) {
+      backBtn.style.display = "none";
+    }
+  }
+});
